37091e8c8dbf95aedc6ee47d747c1bee1787c13f
mauro <olivomauro@gmail.com>
2016-06-03 10:02:55 +0200

    Setup app delegate to work with xibs instead of storyboard

--- CommonsSwift/AppDelegate.swift
+++ CommonsSwift/AppDelegate.swift
@@ -12,10 +12,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var navigationViewController : UINavigationController?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window!.makeKeyAndVisible()
+        
+        self.navigationViewController = UINavigationController(rootViewController: HomeViewController())
+        self.window!.rootViewController = self.navigationViewController
+        
         return true
     }
 
